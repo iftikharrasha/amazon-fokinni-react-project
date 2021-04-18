0526a146d006fb7da129e3ce679292ee269439b1
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-04-18 11:22:28 +0600

    simple routing done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Manage from './components/Manage/Manage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NotFound from './components/NoFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+            <Route path="/shop">
+                <Shop></Shop>
+          	</Route>
+            <Route path="/review">
+               <Review></Review>
+          	</Route>
+            <Route path="/manage">
+                <Manage></Manage>
+          	</Route>
+            <Route exact path="/">
+              <Shop></Shop>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Manage/Manage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Manage.css';
+
+const Manage = () => {
+    return (
+        <div>
+            <h1>This is inventory</h1>
+        </div>
+    );
+};
+
+export default Manage;
